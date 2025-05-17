fdb530d0d9caed0431ee99db2bad963c60eba11b
williamchou500 <williamchou500@gmail.com>
2025-05-16 17:40:38 -0700

    added routes and stations

--- map.js
+++ map.js
@@ -1,13 +1,12 @@
 // Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
 
 // Set your Mapbox access token here
 mapboxgl.accessToken = 'pk.eyJ1Ijoid2lsbGlhbWNob3U1MDAiLCJhIjoiY21hcmdjZjdzMGFubDJqb3MzZzA5OWJzNCJ9.d0SFVMsjtvE9_k9XObORWw';
-
-console.log(mapboxgl.accessToken);
 
 // Initialize the map
 const map = new mapboxgl.Map({
@@ -17,4 +16,53 @@ const map = new mapboxgl.Map({
   zoom: 12, // Initial zoom level
   minZoom: 5, // Minimum allowed zoom
   maxZoom: 18, // Maximum allowed zoom
+});
+
+let jsonData;
+
+map.on('load', async () => {
+  map.addSource('boston_route', {
+  type: 'geojson',
+  data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+});
+
+  map.addLayer({
+  id: 'bike-lanes-boston',
+  type: 'line',
+  source: 'boston_route',
+  paint: {
+  'line-color': '#32D400',  // A bright green using hex code
+  'line-width': 5,          // Thicker lines
+  'line-opacity': 0.6       // Slightly less transparent
+}
+});
+
+  map.addSource('cambridge_route', {
+  type: 'geojson',
+  data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+});
+
+  map.addLayer({
+  id: 'bike-lanes-cambridge',
+  type: 'line',
+  source: 'cambridge_route',
+  paint: {
+  'line-color': 'red',  
+  'line-width': 5,          
+  'line-opacity': 0.6       
+}
+});
+  try {
+    const jsonurl = 'https://dsc106.com/labs/lab07/data/bluebikes-stations.json';
+
+    // Await JSON fetch
+    const jsonData = await d3.json(jsonurl);
+
+    console.log('Loaded JSON Data:', jsonData); // Log to verify structure
+
+    let stations = jsonData.data.stations;
+    console.log('Stations Array:', stations);
+  } catch (error) {
+    console.error('Error loading JSON:', error); // Handle errors
+  }
 });
